e9364d7ac8a5e08603fbd52e12a579b8742c5095
geonoxiy <love.almarie.caranza@student.ateneo.edu>
2024-07-14 15:11:33 +0800

    1

--- excel_web.py
+++ excel_web.py
@@ -2,10 +2,19 @@
 
 import pandas as pd
 import streamlit as st
-import plotly.express as px
+# import plotly.express as px
 
 st.set_page_config(page_title = 'EJEEP TRACKER',
                    layout = 'wide')
+st.header('EJEEP LAST SEEN AT:')
+
+from datetime import datetime
+
+# Get the current time
+current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+# Display the current time in the Streamlit app
+st.write("Current Time:", current_time)
 
 line_a_df = pd.read_excel(
     io = 'ejeep_logbook.xlsx',
