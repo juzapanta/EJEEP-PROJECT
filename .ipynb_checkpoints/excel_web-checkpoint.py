a2d1ccc77e4cc0e111dc396347768d6a3966de6e
geonoxiy <love.almarie.caranza@student.ateneo.edu>
2024-07-14 15:32:41 +0800

    1

--- .ipynb_checkpoints/excel_web-checkpoint.py
+++ .ipynb_checkpoints/excel_web-checkpoint.py
@@ -1,19 +1,15 @@
 import pandas as pd
 import streamlit as st
 # import plotly.express as px
+from datetime import datetime
 
 
 # CURRENT TIME
-from datetime import datetime
-current_date = datetime.now().strftime("%Y-%m-%d")
-current_time = datetime.now().strftime("%H:%M:%S")
+now = datetime.now()
+current_date = now.strftime("%Y-%m-%d")
+current_time = now.strftime("%H:%M:%S")
 st.write("Today is:", current_date)
-st.write("It is currently:", current_time)
-st.header('EJEEP LAST SEEN AT:')
-
-# THE THINGY
-# st.set_page_config(page_title = 'EJEEP TRACKER',
-#                    layout = 'wide')
+st.write("Current Time:", current_time)
 
 # LINE A
 line_a_df = pd.read_excel(
@@ -22,4 +18,13 @@ line_a_df = pd.read_excel(
     sheet_name = 'LINE A',
 )
 
+
+# LINE B
+line_b_df = pd.read_excel(
+    io = 'ejeep_logbook.xlsx',
+    engine = 'openpyxl',
+    sheet_name = 'LINE B',
+)
+
 st.dataframe(line_a_df)
+st.dataframe(line_b_df)
