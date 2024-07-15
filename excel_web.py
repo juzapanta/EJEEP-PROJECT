a328088956e1ee33d735ad8437c5fcbdea6659f0
juzapanta <juliana.zapanta@student.ateneo.edu>
2024-07-15 22:17:09 +0800

    Update excel_web.py

--- excel_web.py
+++ excel_web.py
@@ -1,48 +1,9 @@
-import pandas as pd
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
-import plotly.express as px
-from datetime import datetime
 
+url = "https://docs.google.com/spreadsheets/d/1JDy9md2VZPz4JbYtRPJLs81_3jUK47nx6GYQjgU8qNY/edit?usp=sharing"
 
-# CURRENT TIME
-now = datetime.now()
-current_date = now.strftime("%Y-%m-%d")
-current_time = now.strftime("%H:%M:%S")
-st.write("Today is:", current_date)
-st.write("Current Time:", current_time)
+conn = st.connection("gsheets", type=GSheetsConnection)
 
-st.header('EJEEPS LAST SEEN AT')
-
-conn = st.experimental_connection("gsheets", type = GSheetsConnection)
-
-existing_data = conn.read(worksheet = "Sheet1", usecols=list(range(6)), ttl=5)
-existing_data = existing_data.dropna(how = "all")
-
-st.dataframe(existing_data)
-
-# LINE A
-line_a_df = pd.read_excel(
-    io = 'ejeep_logbook.xlsx',
-    engine = 'openpyxl',
-    sheet_name = 'LINE A',
-)
-
-
-# LINE B
-line_b_df = pd.read_excel(
-    io = 'ejeep_logbook.xlsx',
-    engine = 'openpyxl',
-    sheet_name = 'LINE B',
-)
-
-# EXPRESS
-line_express_df = pd.read_excel(
-    io = 'ejeep_logbook.xlsx',
-    engine = 'openpyxl',
-    sheet_name = 'EXPRESS',
-)
-
-st.dataframe(line_a_df)
-st.dataframe(line_b_df)
-st.dataframe(line_express_df)
+data = conn.read(spreadsheet=url, usecols=[0, 1])
+st.dataframe(data)
