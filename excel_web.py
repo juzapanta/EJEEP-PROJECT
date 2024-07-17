3a3b3ae81e7444d2a7889a21dbd6c9d98ba67860
juzapanta <juliana.zapanta@student.ateneo.edu>
2024-07-17 20:23:29 +0800

    Update excel_web.py

--- excel_web.py
+++ excel_web.py
@@ -6,8 +6,13 @@ sheet_idA = '16CwByzI3-J0o36W7vs4hZ1Ovmyc2uV0DhJH4Cj96rU8'
 
 dfA = pd.read_csv(f"https://docs.google.com/spreadsheets/d/{sheet_idA}/export?format=csv")
 
+st.title("Line A")
 st.write(dfA.head(3))
+
+st.title("Line B")
 st.write(dfA.iloc[5:8])
+
+st.title("Express")
 st.write(dfA.iloc[10:14])
 
 
