5ebed8f21a21bdc569e197fe075f99f49df50748
juzapanta <juliana.zapanta@student.ateneo.edu>
2024-07-17 19:46:04 +0800

    Update excel_web.py

--- excel_web.py
+++ excel_web.py
@@ -1,9 +1,9 @@
 import pandas as pd
+import streamlit as st
 
 sheet_id = '16CwByzI3-J0o36W7vs4hZ1Ovmyc2uV0DhJH4Cj96rU8'
 
 df = pd.read_csv(f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv")
 
-print(df)
-
-print("Hello World")
+st.write("Hello World!")
+st.write(df)
