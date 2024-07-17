55f1cf543bbe0a477d8c5a0dbdc1b582b885016d
juzapanta <juliana.zapanta@student.ateneo.edu>
2024-07-17 19:41:30 +0800

    Update excel_web.py

--- excel_web.py
+++ excel_web.py
@@ -5,3 +5,5 @@ sheet_id = '16CwByzI3-J0o36W7vs4hZ1Ovmyc2uV0DhJH4Cj96rU8'
 df = pd.read_csv(f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv")
 
 print(df)
+
+print("Hello World")
