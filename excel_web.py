05255c0c7c988d02ccd8a6ea5328cf30405555d5
juzapanta <juliana.zapanta@student.ateneo.edu>
2024-07-17 20:01:59 +0800

    Update excel_web.py

--- excel_web.py
+++ excel_web.py
@@ -6,10 +6,7 @@ sheet_id = '16CwByzI3-J0o36W7vs4hZ1Ovmyc2uV0DhJH4Cj96rU8'
 
 df = pd.read_csv(f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv")
 
-def get_data():
-  st.write(df)
+st.write(df)
 
-def main():
-  data = get_data()
-  time.sleep(60 * 1)  # Refresh every 5 minutes (adjust as needed)
-  st.experimental_rerun()
+time.sleep(60 * 1)  # Refresh every 5 minutes (adjust as needed)
+st.experimental_rerun()
