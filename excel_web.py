b02052a1ee6732b5065bfc83d191b3fed57c3e29
juzapanta <juliana.zapanta@student.ateneo.edu>
2024-07-17 20:21:18 +0800

    Update excel_web.py

--- excel_web.py
+++ excel_web.py
@@ -6,9 +6,9 @@ sheet_idA = '16CwByzI3-J0o36W7vs4hZ1Ovmyc2uV0DhJH4Cj96rU8'
 
 dfA = pd.read_csv(f"https://docs.google.com/spreadsheets/d/{sheet_idA}/export?format=csv")
 
-st.write(dfA.head(4))
-st.write(dfA.iloc[6:9])
-st.write(dfA.iloc[11:14])
+st.write(dfA.head(3))
+st.write(dfA.iloc[5:9])
+st.write(dfA.iloc[10:14])
 
 
 time.sleep(60 * 1)  # Refresh every 5 minutes (adjust as needed)
