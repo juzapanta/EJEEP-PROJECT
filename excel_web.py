bff97e314c63c942caa361148f6a6aaf5d7046df
juzapanta <juliana.zapanta@student.ateneo.edu>
2024-07-17 19:58:21 +0800

    Update excel_web.py

--- excel_web.py
+++ excel_web.py
@@ -1,9 +1,15 @@
 import pandas as pd
 import streamlit as st
+import time
 
 sheet_id = '16CwByzI3-J0o36W7vs4hZ1Ovmyc2uV0DhJH4Cj96rU8'
 
 df = pd.read_csv(f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv")
 
-st.write("Hello World!")
-st.write(df)
+def get_data():
+  st.write(df)
+
+def main():
+  data = get_data()
+  time.sleep(60 * 1)  # Refresh every 5 minutes (adjust as needed)
+  st.experimental_rerun()
