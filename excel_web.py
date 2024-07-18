411c3df95e38d03cb28b30d18da0b330098b4a99
Madieline Dini-ay <madieline.nicole.dini-ay@student.ateneo.edu>
2024-07-18 17:33:55 +0800

    Update excel_web.py

--- excel_web.py
+++ excel_web.py
@@ -46,18 +46,18 @@ if line == "LINE A":
     st.title("Line A")
     
     # A1
-    A1 = df.iloc[1, 1] 
+    A1 = dfA.iloc[1, 1] 
     plot_map("A1", A1, line_coords["LINE A"]["coords"], line_coords["LINE A"]["place_coords"], line_coords["LINE A"]["place_labels"])
     st.pyplot(plt)
     
 
     # A2
-    A2 = df.iloc[2, 1]  
+    A2 = dfA.iloc[2, 1]  
     plot_map("A2", A2, line_coords["LINE A"]["coords"], line_coords["LINE A"]["place_coords"], line_coords["LINE A"]["place_labels"])
     st.pyplot(plt)
 
     # A3
-    A3 = df.iloc[3, 1]  
+    A3 = dfA.iloc[3, 1]  
     plot_map("A3", A3, line_coords["LINE A"]["coords"], line_coords["LINE A"]["place_coords"], line_coords["LINE A"]["place_labels"])
     st.pyplot(plt)
 
