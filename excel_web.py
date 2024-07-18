35a8d219d64a88a541592a152de0220f14ca025e
Madieline Dini-ay <madieline.nicole.dini-ay@student.ateneo.edu>
2024-07-18 13:05:27 +0800

    Update excel_web.py

--- excel_web.py
+++ excel_web.py
@@ -6,6 +6,8 @@ sheet_idA = '16CwByzI3-J0o36W7vs4hZ1Ovmyc2uV0DhJH4Cj96rU8'
 
 dfA = pd.read_csv(f"https://docs.google.com/spreadsheets/d/{sheet_idA}/export?format=csv")
 
+st.selectbox(label="Choose E-jeep Line to view", options=["LINE A", "LINE B", "EXPRESS"])
+
 st.set_page_config(layout="centered")
 
 st.image("https://cdn-icons-png.flaticon.com/512/9249/9249336.png", width=100)
