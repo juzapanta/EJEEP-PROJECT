8cfb1e7e6ade4226a5d07d21d45b00e36838cae5
Madieline Dini-ay <madieline.nicole.dini-ay@student.ateneo.edu>
2024-07-18 17:31:52 +0800

    Update excel_web.py

--- excel_web.py
+++ excel_web.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import streamlit as st
 import time
+import matplotlib
+from matplotlib import pyplot as plt
+from matplotlib.offsetbox import OffsetImage, AnnotationBbox
 
 sheet_idA = '16CwByzI3-J0o36W7vs4hZ1Ovmyc2uV0DhJH4Cj96rU8'
 
