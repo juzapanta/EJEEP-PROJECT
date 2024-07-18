7d4adc8bbea72ce0e0a9b002f57cc76dc8f70d28
Madieline Dini-ay <madieline.nicole.dini-ay@student.ateneo.edu>
2024-07-18 15:23:39 +0800

    Update excel_web.py

--- excel_web.py
+++ excel_web.py
@@ -2,7 +2,7 @@ import pandas as pd
 import streamlit as st
 import time
 
-sheet_id = '16CwByzI3-J0o36W7vs4hZ1Ovmyc2uV0DhJH4Cj96rU8'
+sheet_idA = '16CwByzI3-J0o36W7vs4hZ1Ovmyc2uV0DhJH4Cj96rU8'
 
 dfA = pd.read_csv(f"https://docs.google.com/spreadsheets/d/{sheet_idA}/export?format=csv")
 
