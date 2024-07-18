ec9fcf718016c40c7d0808ccd573d7980209ccac
Madieline Dini-ay <madieline.nicole.dini-ay@student.ateneo.edu>
2024-07-18 20:44:57 +0800

    Update excel_web.py

--- excel_web.py
+++ excel_web.py
@@ -46,14 +46,14 @@ def highlight_route(ax, start, end, line_coords):
 
 line_coords = {
     "LINE A": {
-        "coords": [(13, 1), (10, 1), (10, 3), (10, 4), (8, 4), (8, 4.5), (11, 4.5), (11, 5), (12.5, 5), (14.5, 5), (15, 5), (13, 1)],
-        "place_coords": [(13, 1), (10, 3), (8, 4), (11, 4.5), (12.5, 5), (14.5, 5)],
-        "place_labels": ['Hagdan na Bato', 'Old Comm', 'Gate 1', 'Gate 2.5', 'Leong Hall', 'Xavier Hall']
+        "coords": [(15, 1), (10, 1), (10, 3), (10, 4), (8, 4), (8, 4.5), (11, 4.5), (11, 5), (12.5, 5), (14.5, 5), (15, 5), (15, 1)],
+        "place_coords": [(15, 1), (10, 3), (8, 4), (11, 4.5), (12.5, 5), (14.5, 5), (15, 1)],
+        "place_labels": ['Hagdan na Bato', 'Old Comm', 'Gate 1', 'Gate 2.5', 'Leong Hall', 'Xavier Hall', 'Hagdan na Bato']
     }, 
     "LINE B":{
         "coords": [(11,1),(15,1),(15,10),(15,17),(15,20),(14,20),(14,7.5),(10,7.5),(8,7.5),(8,5),(9,5),(9,1),(11,1)],
-        "place_coords": [(11,1),(15,10),(15,17),(10,7.5),(8,5)],
-        "place_labels": ['Xavier Hall','AJHS','ASHS FLC Building','ISO','Arete']
+        "place_coords": [(11,1),(15,10),(15,17),(10,7.5),(8,5), (11, 1)],
+        "place_labels": ['Xavier Hall','AJHS','ASHS FLC Building','ISO','Arete', 'Xavier Hall']
     }
 }
 
@@ -84,6 +84,9 @@ if line == "LINE A":
         st.pyplot(fig)
     except Exception as e:
         st.error(f"Error highlighting route: {e}")
+
+    if dfA.iloc[0, 3] == "For Charging":
+        st.write('This E-jeep is only until Gate 1.')
     
     # A2
     A2 = dfA.iloc[1, 1]  
@@ -109,6 +112,9 @@ if line == "LINE A":
     except Exception as e:
         st.error(f"Error highlighting route: {e}")
 
+    if dfA.iloc[1, 3] == "For Charging":
+        st.write('This E-jeep is only until Gate 1.')
+
     # A3
     A3 = dfA.iloc[2, 1]  
     fig, ax = plot_map("A3", A3, line_coords["LINE A"]["coords"], line_coords["LINE A"]["place_coords"], line_coords["LINE A"]["place_labels"])
@@ -132,6 +138,9 @@ if line == "LINE A":
         st.pyplot(fig)
     except Exception as e:
         st.error(f"Error highlighting route: {e}")
+
+    if dfA.iloc[2, 3] == "For Charging":
+        st.write('This E-jeep is only until Gate 1.')
 
 if line == "LINE B":
     st.title("Line B")
@@ -159,6 +168,9 @@ if line == "LINE B":
     except Exception as e:
         st.error(f"Error highlighting route: {e}")
     
+    if dfA.iloc[5, 3] == "For Charging":
+        st.write('This E-jeep is only until Gate 1.')
+    
     # B2
     B2 = dfA.iloc[6, 1]  
     fig, ax = plot_map("B2", B2, line_coords["LINE B"]["coords"], line_coords["LINE B"]["place_coords"], line_coords["LINE B"]["place_labels"])
@@ -180,6 +192,9 @@ if line == "LINE B":
         st.pyplot(fig)
     except Exception as e:
         st.error(f"Error highlighting route: {e}")
+
+    if dfA.iloc[6, 3] == "For Charging":
+        st.write('This E-jeep is only until Gate 1.')
     
     # B3
     B3 = dfA.iloc[7, 1]  
@@ -202,6 +217,9 @@ if line == "LINE B":
         st.pyplot(fig)
     except Exception as e:
         st.error(f"Error highlighting route: {e}")
+
+    if dfA.iloc[7, 3] == "For Charging":
+        st.write('This E-jeep is only until Gate 1.')
 
 time.sleep(60 * 1) 
 st.experimental_rerun()
